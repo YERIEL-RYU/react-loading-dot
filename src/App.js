394e1cb4a283a41810cb191a8fb17b89e4f5f6cb
YERIEL-RYU <yeriel.ryu@gmail.com>
2022-01-11 17:59:25 +0900

    styled-component loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import './App.scss'
+import styled, {keyframes} from 'styled-components';
+
+const wave = keyframes`
+  50%,
+  75% { transform: scale(2.5); }
+  80%,
+  100% { opacity: 0; }
+`;
+const Loading = styled.div`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const Dot = styled.div`
+  position: relative;
+  width: 1em;
+  height: 1em;
+  margin: 1em;
+  border-radius: 50%;
+  &:before{
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background: inherit;
+    border-radius: inherit;
+    animation: ${wave} 2s ease-out infinite;
+  }
+  background-color: ${(props)=>props.color};
+  &:before{
+    animation-delay: ${(props)=>props.index * 0.3+'s'};
+  }
+`;
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -9,18 +43,18 @@ const App = () => {
       setLoading(false)
     }, 5000)
   },[]);
-  
+
   return (
     <>
       {
         loading? (
-          <div className='loading'>
-            <div className='dot'></div>
-            <div className='dot'></div>
-            <div className='dot'></div>
-            <div className='dot'></div>
-            <div className='dot'></div>
-          </div>
+          <Loading>
+            <Dot className="dot" color='#7ef9ff' index='1'></Dot>
+            <Dot className="dot" color='#89cff0' index='2'></Dot>
+            <Dot className="dot" color='#4682b4' index='3'></Dot>
+            <Dot className="dot" color='#0f52ba' index='4'></Dot>
+            <Dot className="dot" color='#000080' index='5'></Dot>
+          </Loading>
         ) : (
           <h1>Hello YERIEL</h1>
       )}
